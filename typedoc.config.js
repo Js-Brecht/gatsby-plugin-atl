d7d602ff12db337abcf1e662fa37ef102bd662f7
Jeremy Albright <jeremy.albright@manaollc.com>
2019-08-04 12:41:10 -1000

    Updated documentation theme

---
+++ typedoc.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const themePath = path.resolve(path.join(".", "theme"));
+
+module.exports = {
+    "out": "docs",
+    "target": "es6",
+    "theme": themePath,
+    "readme": "README.md",
+    "mode": "file",
+    "hideGenerator": true,
+}
