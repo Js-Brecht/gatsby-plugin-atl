2d4de5bd72122be81675d9735a62ef1d56c2edd2
Jeremy Albright <myrlin1@manaollc.com>
2019-10-22 10:20:45 -1000

    typedoc updates

--- typedoc.config.js
+++ typedoc.config.js
@@ -3,10 +3,13 @@ const path = require('path');
 const themePath = path.resolve(path.join(".", "theme"));
 
 module.exports = {
-    "out": "docs",
-    "target": "es6",
-    "theme": themePath,
-    "readme": "README.md",
-    "mode": "file",
-    "hideGenerator": true,
-}
+	"out": "docs",
+	"target": "es6",
+	"theme": themePath,
+	"readme": "README.md",
+	"mode": "file",
+	"hideGenerator": true,
+	"excludeExternals": true,
+	"excludePrivate": true,
+	"excludeNotExported": true,
+};
